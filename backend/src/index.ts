496841888b854ca74c3cd353231f012b1b8067ba
vstefanescu <vstefanescu@stud.acs.upb.ro>
2025-06-11 02:48:05 +0300

    test

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
+
 import productRoutes from './routes/productRoutes';
 import authroutes from './routes/authRoutes';
 import userRoutes from './routes/userRoutes';
@@ -9,14 +10,13 @@ dotenv.config();
 const app = express();
 
 app.use(cors({
-  origin: 'https://e-commerce-frontend-jade-two.vercel.app',
+  origin: ['https://e-commerce-frontend-jade-two.vercel.app'],
   credentials: true
 }));
-
 app.use(express.json());
+
 app.use('/api/auth', authroutes);
 app.use('/api', userRoutes);
-
 app.use('/api/products', productRoutes);
 
 app.get('/api/hello', (req, res) => {
