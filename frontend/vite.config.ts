b3d5d6f2870128389904097e15b695a454bc43b2
vstefanescu <vstefanescu@stud.acs.upb.ro>
2025-06-15 00:15:37 +0300

    Fix API URL for web

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,8 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  css: {
+    postcss: './postcss.config.js',
+  },
   server: {
     proxy: {
       '/api': {
@@ -11,4 +14,4 @@ export default defineConfig({
       },
     },
   },
-})
+});
